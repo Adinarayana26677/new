56ee254fc5bb215b4803362ebc6f0317073d731c
Adi <adi@gmail.com>
2016-06-02 16:48:10 +0000

    Initial Commit

---
+++ testeclipse/src/testeclipse/testeclipse.java
@@ -0,0 +1,11 @@
+package testeclipse;
+
+public class testeclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+
+	}
+
+}
